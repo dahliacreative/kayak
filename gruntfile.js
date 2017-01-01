51af64c7622ff3d81db00f981d3f919aeb50dd64
dahliacreative <simon@dahliacreative.com>
2017-01-01 12:09:12 +0000

    Initial Commit

---
+++ gruntfile.js
@@ -0,0 +1,136 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: ['app']
+    },
+
+    sass: {
+      build: {
+        options: {
+          style: 'compressed',
+          loadPath: require('node-bourbon').includePaths,
+          require: 'sass-globbing'
+        },
+        files: {
+          'app/stylesheets/application.css': 'src/stylesheets/application.sass'
+        }
+      },
+      dist: {
+        options: {
+          style: 'compressed',
+          loadPath: require('node-bourbon').includePaths,
+          require: 'sass-globbing',
+          sourcemap: 'none'
+        },
+        files: {
+          'app/stylesheets/application.css': 'src/stylesheets/application.sass'
+        }
+      }
+    },
+
+    concat: {
+      build: {
+        options: {
+          sourceMap: true
+        },
+        files: {
+          'app/javascript/application.js' : [
+          'src/javascript/vendor/*.js',
+          'src/javascript/modules/*.js',
+          'src/javascript/application.js'
+          ],
+        }
+      },
+      dist: {
+        options: {
+          sourceMap: false
+        },
+        files: {
+          'app/javascript/application.js' : [
+          'src/javascript/vendor/*.js',
+          'src/javascript/modules/*.js',
+          'src/javascript/application.js'
+          ],
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        sourceMap: false,
+      },
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'app/javascript',
+          src: ['**/*.js'],
+          dest: 'app/javascript'
+        }]
+      }
+    },
+
+    imagemin: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'src/images',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: 'app/images'
+        }]
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['src/stylesheets/**/*'],
+        tasks: ['sass:build'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      },
+      js: {
+        files: ['src/javascript/**/*.js'],
+        tasks: ['concat:build'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      },
+      html: {
+        files: ['index.html'],
+        tasks: [],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      },
+      images: {
+        files: ['src/images/**/*'],
+        tasks: ['imagemin'],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
+
+  });
+
+  // Load Tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+  // Default task(s).
+  grunt.registerTask('default', ['clean', 'sass:build', 'concat:build', 'imagemin', 'watch']);
+  grunt.registerTask('production', ['clean', 'sass:dist', 'concat:dist', 'uglify', 'imagemin']);
+
+};
